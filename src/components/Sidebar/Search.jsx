8e04742c0386678fde5613c3c97c6c6ef3f53666
devindhi <136802380+devindhi@users.noreply.github.com>
2024-02-26 15:24:50 +0530

    modified

--- src/components/Sidebar/Search.jsx
+++ src/components/Sidebar/Search.jsx
@@ -63,7 +63,7 @@ const Search = () => {
 						<form onSubmit={handleSearchUser}>
 							<FormControl>
 								<FormLabel>Username</FormLabel>
-								<Input placeholder='asaprogrammer' ref={searchRef} />
+								<Input placeholder='Enter a username' ref={searchRef} />
 							</FormControl>
 
 							<Flex w={"full"} justifyContent={"flex-end"}>
